e7f36d43df74bc8a912e4ec17f12a461eb9f6bfc
Alena Pumprová <alenapumprova@seznam.cz>
2026-01-16 07:24:20 +0100

    addint the bet toggle

--- src/components/BottomPannel.tsx
+++ src/components/BottomPannel.tsx
@@ -1,7 +1,10 @@
 
 import "./BottomPannel.css"
+import { useState } from "react";
+import { BET_OPTIONS } from "../data/data";
 import { FaVolumeUp, FaVolumeMute, FaCaretUp, FaCaretDown } from "react-icons/fa";
 import {FaArrowsRotate, FaArrowRotateRight, FaBars } from "react-icons/fa6";
+
 
 type BottomPannelProps = {
     isSoundOn: boolean;
@@ -9,16 +12,32 @@ type BottomPannelProps = {
 }
 
 const BottomPannel = ({isSoundOn, setIsSoundOn}:BottomPannelProps) => {
+    const [bet, setBet] = useState(BET_OPTIONS[0]);
+
+    const increaseBet = () => {
+        const currentIndex = BET_OPTIONS.indexOf(bet);
+        if (currentIndex < BET_OPTIONS.length - 1) {
+            setBet(BET_OPTIONS[currentIndex + 1]);
+        }
+    };
+
+    const decreaseBet = () => {
+        const currentIndex = BET_OPTIONS.indexOf(bet);
+        if (currentIndex > 0) {
+            setBet(BET_OPTIONS[currentIndex - 1]);
+        }
+    };
+
     return (
             <div className="bottom-panel">
                 <button className="menu"><FaBars /></button>
                 <button className="sound" onClick={() => setIsSoundOn(!isSoundOn)}>{isSoundOn ? <FaVolumeMute /> : <FaVolumeUp />}</button>
                 <div className="credit">Credit:<br />1000</div>
                 <div className="bet">
-                    <span className="amount">Bet:<br />20</span>
+                    <span className="amount">Bet:<br />{bet}</span>
                     <div className="amount-toggle">
-                        <button className="increase"><FaCaretUp /></button>
-                        <button className="decrease"><FaCaretDown /></button>
+                        <button className="increase" onClick={increaseBet}><FaCaretUp /></button>
+                        <button className="decrease" onClick={decreaseBet}><FaCaretDown /></button>
                     </div>
                 </div>
                 <div className="win">Win:<br />0</div>
